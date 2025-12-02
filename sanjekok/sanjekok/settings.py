20d07177b48ef7e9f456708f41259a20a098329c
imyongchan <yongchan01045@naver.com>
2025-12-02 17:54:11 +0900

    stats api 연결

--- sanjekok/sanjekok/settings.py
+++ sanjekok/sanjekok/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 KAKAO_KEY = os.getenv("KAKAO_KEY")
+KOSIS_API_KEY = os.getenv("KOSIS_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
